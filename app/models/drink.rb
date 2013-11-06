1f361d88a289471de20f6d71c0baa5a1ac9ff143
Rolen Le <rolentle@gmail.com>
2013-11-05 17:26:43 -0700

    Added drink  validation test

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -1,3 +1,6 @@
 class Drink < ActiveRecord::Base
   belongs_to :category
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :description, presence: true, length: { maximum: 140 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
--- test/models/drink_test.rb
+++ test/models/drink_test.rb
@@ -7,4 +7,46 @@ class DrinkTest < ActiveSupport::TestCase
     beer = Drink.create(name: "Dale's Pale Ale", category_id: beer_cat.id)
     assert_equal beer_cat.id, beer.category_id
   end
+
+  def test_drink_name_must_be_presence
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "", description: "WTF?", price: 1.00
+    end
+  end
+
+  def test_drink_name_cant_longer_than_25
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "Q" * 26, description: "bad stuff", price: 4.00
+    end
+  end
+
+  def test_drink_description_must_be_presence
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "Diet Coke", description: "", price: 2.99
+    end
+  end
+
+  def test_drink_description_cant_be_longer_than_140_characters
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "Water", description: "Boring" * 100, price: 5
+    end
+  end
+
+  def test_drink_price_has_to_be_present
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "Tea", description: "Ok, I guess"
+    end
+  end
+
+  def test_drink_price_has_to_be_a_number
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "Tea", description: "Ok, I guess", price: "string"
+    end
+  end
+
+  def test_drink_price_cant_be_negative
+    assert_raise ActiveRecord::RecordInvalid do
+      Drink.create! name: "Tea", description: "Ok, I guess", price: -1
+    end
+  end
 end
