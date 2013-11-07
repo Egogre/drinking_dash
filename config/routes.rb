91e672d347b89ee02716c86dc936178804f1c5dd
Rolen Le <rolentle@gmail.com>
2013-11-06 18:37:58 -0700

    Added Login Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ DrinkingDash::Application.routes.draw do
   get "users/new"
   resources :drinks
   resources :categories
+  resources :users
+  match '/signup', to: 'users#new', via: 'get'
   root 'drinks#index'
 
 end
