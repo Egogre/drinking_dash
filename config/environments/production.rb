ddc409cc4be6b7e00731b0e6af2bf469b6e9bf93
Rolen Le <rolentle@gmail.com>
2013-11-12 16:28:51 -0700

    Fixing Images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ DrinkingDash::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-   config.action_controller.asset_host = "http://s3-us-west-2.amazonaws.com/drinking-dash/"
+  # config.action_controller.asset_host = "http://s3-us-west-2.amazonaws.com/drinking-dash/"
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
