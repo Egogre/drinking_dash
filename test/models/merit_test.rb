d60c3d689b525f5f622a02a3ff184e4376d7004f
Meeka <rrgayhart@gmail.com>
2013-11-13 11:07:03 -0700

    Adds testing for unique badge verification

--- test/models/merit_test.rb
+++ test/models/merit_test.rb
@@ -2,21 +2,36 @@ require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
 
+  def u_array
+    {name: "SteveBob", email: "stevebob@example.com", password: "stuff111", password_confirmation: "stuff111"}
+  end
+
   def test_new_user_has_no_badges_at_first
-    user = User.create name: "SteveBob", email: "stevebob@example.com", password: "stuff111", password_confirmation: "stuff111"
+    user = User.create u_array
     assert_equal [], user.badges
   end
 
   def test_newbie_badge_can_be_added_manually
-    user = User.create name: "SteveBob", email: "stevebob@example.com", password: "stuff111", password_confirmation: "stuff111"
+    user = User.create u_array
     user.add_badge(1)
     assert_equal "newbie", user.badges.first.name
   end
 
   def test_badge_has_image_and_description
-    user = User.create name: "SteveBob", email: "stevebob@example.com", password: "stuff111", password_confirmation: "stuff111"
+    user = User.create u_array
     user.add_badge(1)
     assert_includes user.badges.first.description, "New User"
     assert_includes user.badges.first.custom_fields, ".png"
+  end
+
+  def test_user_add_unique_id_will_not_duplicate_badges
+    user = User.create u_array
+    assert_equal user.badges, []
+    user.unique_badge_add(1)
+    assert_equal user.badges.count, 1
+    user.unique_badge_add(2)
+    assert_equal user.badges.count, 2
+    user.unique_badge_add(2)
+    assert_equal user.badges.count, 2
   end
 end
